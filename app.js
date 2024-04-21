22059d35648c41e5d6991eb3106a4b138b454ba8
Seungyeon Chang <schang10@una.edu>
2024-04-20 21:09:54 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello and Welcome!'));
+app.get('/', (req, res) => res.send('Hello and Welcome! I am Seungyeon!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
